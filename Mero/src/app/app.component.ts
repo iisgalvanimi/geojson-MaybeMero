4c37a390d1d4009e20b459030481221cb32133b8
MaybeMero <mero.goyes.maybe.yuleysy@iisgalvanimi.edu.it>
2020-04-08 20:08:52 +0000

    proggetto completato fino alla visualizzazione dei  marker

---
+++ Mero/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {GEOJSON, GeoFeatureCollection } from './models/geojson.model';
+import { Marker} from './models/marker.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Mero';
+// google maps zoom level
+  zoom: number = 8;
+  geoJsonObject : GeoFeatureCollection;
+  fillColor: string = "#FF0000";
+  markers: Marker[];
+
+  lng: number = 9.02657833507687;
+  lat: number = 45.512051524024308;
+
+  constructor()
+  {
+    this.geoJsonObject = GEOJSON;
+    console.log(this.geoJsonObject);
+    console.log(this.lng,this.lat);
+  }
+  styleFunc = (feature) =>{
+    console.log(feature.i.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+  ngOnInit() {
+    this.markers = [
+      {
+
+        lng: this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+        lat: this.geoJsonObject.features[0].geometry.coordinates[0][0][1],
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        lng: this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+        lat: this.geoJsonObject.features[1].geometry.coordinates[0][0][1],
+        label: String(this.geoJsonObject.features[1].properties.id),
+      }
+    ]
+  }
+
+}
---
+++ Mero/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+     AgmCoreModule.forRoot({apiKey:  'AIzaSyC-fsfM-piCvdrloo30qlqtnZyX2SgBpCg'}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
